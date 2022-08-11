6d1b16b38d1e471f36dba05868d03bd895d64f57
xxxmkxxx <maks88648@gmail.com>
2022-08-11 11:44:40 +0400

    Rename interface

--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/renderer/NodeRenderer.java
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/renderer/NodeRenderer.java
@@ -2,6 +2,7 @@ package com.xxxmkxxx.customgui.client.hierarchy.renderer;
 
 import com.xxxmkxxx.customgui.client.hierarchy.node.Node;
 
-public interface Renderer <N extends Node>{
+@FunctionalInterface
+public interface NodeRenderer<N extends Node>{
     void render(N node);
 }
