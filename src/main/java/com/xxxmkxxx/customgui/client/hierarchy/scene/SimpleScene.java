e2ad731da7301d64e01d2fbc6a618bda43a5b6a8
xxxmkxxx <maks88648@gmail.com>
2022-08-12 17:51:40 +0400

    Changed constructors for integration RendererType

--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/scene/SimpleScene.java
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/scene/SimpleScene.java
@@ -1,21 +1,20 @@
 package com.xxxmkxxx.customgui.client.hierarchy.scene;
 
-import com.xxxmkxxx.customgui.client.hierarchy.Node;
+import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
+import com.xxxmkxxx.customgui.client.hierarchy.renderer.RendererType;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 
 public class SimpleScene extends AbstractScene {
-    public SimpleScene() {
+    public SimpleScene(RendererType type) {
+        super(type);
         this.displays = new LinkedList<>();
     }
 
-    public SimpleScene(Node... displays) {
-        this.displays = new LinkedList<>(Arrays.asList(displays));
-    }
-
-    @Override
-    public void render() {
-        displays.forEach(Node::view);
+    public SimpleScene(RendererType type, AbstractNode ... displays) {
+        super(type);
+        this.displays = new LinkedList<>();
+        Arrays.stream(displays).forEach(this::addElement);
     }
 }
