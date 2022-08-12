4e5572bf7d0128508e7ada5316a321af9e8ce9dd
xxxmkxxx <maks88648@gmail.com>
2022-08-12 18:32:37 +0400

    Added HudStage static initialization; Regiter HudStage render method.

--- src/main/java/com/xxxmkxxx/customgui/client/CustomGUIClient.java
+++ src/main/java/com/xxxmkxxx/customgui/client/CustomGUIClient.java
@@ -1,13 +1,18 @@
 package com.xxxmkxxx.customgui.client;
 
+import com.xxxmkxxx.customgui.client.hierarchy.stage.HudStage;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class CustomGUIClient implements ClientModInitializer {
+    public static final HudStage HUD_STAGE = new HudStage();
+
     @Override
     public void onInitializeClient() {
         HudRenderCallback.EVENT.register((matrixStack, tickDelta) -> {
-            TestHud.render(matrixStack);
+            HUD_STAGE.render();
         });
+
+        TestHud.render();
     }
 }
