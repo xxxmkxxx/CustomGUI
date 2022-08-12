50096ff61726ed449e276ce372e56acb322a06f4
xxxmkxxx <maks88648@gmail.com>
2022-08-12 18:21:59 +0400

    Added AbstractPane extend AbstractNode; Changed nodes List type; Removed state field.

--- src/main/java/com/xxxmkxxx/customgui/client/ui/containers/pane/AbstractPane.java
+++ src/main/java/com/xxxmkxxx/customgui/client/ui/containers/pane/AbstractPane.java
@@ -1,14 +1,15 @@
 package com.xxxmkxxx.customgui.client.ui.containers.pane;
 
 import com.xxxmkxxx.customgui.client.common.Frame;
-import com.xxxmkxxx.customgui.client.hierarchy.Node;
+import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
+import lombok.Getter;
 import net.minecraft.client.util.math.MatrixStack;
 
 import java.util.List;
 
-public abstract class AbstractPane implements Pane {
-    protected List<Node> nodes;
-    protected PaneState state;
+@Getter
+public abstract class AbstractPane extends AbstractNode implements Pane {
+    protected List<AbstractNode> nodes;
     protected MatrixStack matrixStack;
     protected Frame frame;
 }
