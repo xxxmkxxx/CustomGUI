a1046167bacfeb8e912701dc6e835ccb48add326
xxxmkxxx <maks88648@gmail.com>
2022-08-12 17:44:43 +0400

    Added renderer type field; Added displays list; Added implementation addElement method; Added render implementation method.

--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/scene/AbstractScene.java
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/scene/AbstractScene.java
@@ -1,9 +1,26 @@
 package com.xxxmkxxx.customgui.client.hierarchy.scene;
 
-import com.xxxmkxxx.customgui.client.hierarchy.Node;
+import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
+import com.xxxmkxxx.customgui.client.hierarchy.renderer.RendererType;
 
 import java.util.List;
 
 public abstract class AbstractScene implements Scene {
-    protected List<Node> displays;
+    protected final RendererType type;
+    protected List<AbstractNode> displays;
+
+    public AbstractScene(RendererType type) {
+        this.type = type;
+    }
+
+    @Override
+    public void addElement(AbstractNode node) {
+        node.initRenderer(type);
+        displays.add(node);
+    }
+
+    @Override
+    public void render() {
+        displays.forEach(node -> node.getState().execute(node, node.getRenderer()));
+    }
 }
