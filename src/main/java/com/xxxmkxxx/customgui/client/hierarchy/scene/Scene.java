f270ad5bda0fc258ca2da3f2411d769b72e7a910
xxxmkxxx <maks88648@gmail.com>
2022-08-12 17:39:35 +0400

    Change addElement method signature

--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/scene/Scene.java
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/scene/Scene.java
@@ -1,5 +1,8 @@
 package com.xxxmkxxx.customgui.client.hierarchy.scene;
 
+import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
+
 public interface Scene {
+    void addElement(AbstractNode node);
     void render();
 }
