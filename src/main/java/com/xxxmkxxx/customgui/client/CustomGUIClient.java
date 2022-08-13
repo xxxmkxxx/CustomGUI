3ee327661c23269f95d2ec9d35fdb373528a74cd
xxxmkxxx <maks88648@gmail.com>
2022-08-13 17:48:28 +0400

    Added node drawable helper class initializatiio

--- src/main/java/com/xxxmkxxx/customgui/client/CustomGUIClient.java
+++ src/main/java/com/xxxmkxxx/customgui/client/CustomGUIClient.java
@@ -1,16 +1,20 @@
 package com.xxxmkxxx.customgui.client;
 
+import com.xxxmkxxx.customgui.client.hierarchy.node.NodeDrawableHelper;
 import com.xxxmkxxx.customgui.client.hierarchy.stage.HudStage;
+import com.xxxmkxxx.customgui.client.hierarchy.stage.ScreenStage;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class CustomGUIClient implements ClientModInitializer {
+    public static final NodeDrawableHelper NODE_DRAWABLE_HELPER = new NodeDrawableHelper();
     public static final HudStage HUD_STAGE = new HudStage();
+    public static final ScreenStage SCREEN_STAGE = new ScreenStage();
 
     @Override
     public void onInitializeClient() {
         HudRenderCallback.EVENT.register((matrixStack, tickDelta) -> {
-            HUD_STAGE.render();
+            SCREEN_STAGE.render();
         });
 
         TestHud.render();
