c76e89992f0ce1e3f70d1e79969190da99c9f6d2
xxxmkxxx <maks88648@gmail.com>
2022-08-13 19:48:46 +0400

    Added gradient color fields; Added gradientFillFrame methods; Added fillFrame methods.

--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/node/NodeDrawableHelper.java
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/node/NodeDrawableHelper.java
@@ -1,10 +1,40 @@
 package com.xxxmkxxx.customgui.client.hierarchy.node;
 
+import com.xxxmkxxx.customgui.client.common.Frame;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.util.math.MatrixStack;
 
 public class NodeDrawableHelper extends DrawableHelper {
-    public void gradient(MatrixStack matrices, int startX, int startY, int endX, int endY, int colorStart, int colorEnd) {
-        fillGradient(matrices, startX, startY, endX, endY, colorStart, colorEnd);
+    public static int startGradientColor = -1072689136;
+    public static int stopGradientColor = -804253680;
+
+    public void gradientFillFrame(MatrixStack matrices, int startX, int startY, int stopX, int stopY, int colorStart, int colorEnd) {
+        fillGradient(matrices, startX, startY, stopX, stopY, colorStart, colorEnd);
+    }
+
+    public void gradientFillFrame(MatrixStack matrices, int startX, int startY, int stopX, int stopY) {
+        fillGradient(matrices, startX, startY, stopX, stopY, startGradientColor, stopGradientColor);
+    }
+
+    public void gradientFillFrame(MatrixStack matrices, Frame frame, int colorStart, int colorEnd) {
+        gradientFillFrame(
+                matrices,
+                frame.getStartPos().x(), frame.getStartPos().y(),
+                frame.getStopPos().x(), frame.getStopPos().y(),
+                colorStart, colorEnd
+        );
+    }
+
+    public void fillFrame(MatrixStack matrices, int startX, int startY, int stopX, int stopY, int color) {
+        fill(matrices, startX, startY, stopX, stopY, color);
+    }
+
+    public void fillFrame(MatrixStack matrices, Frame frame, int color) {
+        fillFrame(
+                matrices,
+                frame.getStartPos().x(), frame.getStartPos().y(),
+                frame.getStopPos().x(), frame.getStopPos().y(),
+                color
+        );
     }
 }
