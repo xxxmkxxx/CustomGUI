1bcb3f4d54adfe9068cb700f466128e7e366cd7f
xxxmkxxx <maks88648@gmail.com>
2022-08-14 13:49:38 +0400

    Added field for change location

--- src/main/java/com/xxxmkxxx/customgui/client/common/Frame.java
+++ src/main/java/com/xxxmkxxx/customgui/client/common/Frame.java
@@ -8,8 +8,11 @@ import lombok.Getter;
 public class Frame {
     private final Pos startPos;
     private final Pos stopPos;
+    private final boolean startInCenter;
 
     public Frame(int xPos, int yPos, int width, int height, boolean startInCenter) {
+        this.startInCenter = startInCenter;
+
         if (startInCenter) {
             this.startPos = new Pos(xPos - width / 2, yPos - height / 2);
             this.stopPos = new Pos(xPos + width / 2, yPos + height / 2);
