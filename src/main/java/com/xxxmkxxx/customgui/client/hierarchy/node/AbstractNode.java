f1f459640f2dfa4708aae8d121a679847915a0ca
xxxmkxxx <maks88648@gmail.com>
2022-08-14 13:52:14 +0400

    Added default state

--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/node/AbstractNode.java
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/node/AbstractNode.java
@@ -3,10 +3,12 @@ package com.xxxmkxxx.customgui.client.hierarchy.node;
 import com.xxxmkxxx.customgui.client.hierarchy.renderer.NodeRenderer;
 import com.xxxmkxxx.customgui.client.hierarchy.renderer.RendererType;
 import lombok.Getter;
+import net.minecraft.client.util.math.MatrixStack;
 
 @Getter
 public abstract class AbstractNode implements Node {
-    protected NodeState<AbstractNode> state;
+    protected MatrixStack matrixStack = new MatrixStack();
+    protected NodeState<AbstractNode> state = States.DISPLAYED;
     protected NodeRenderer renderer;
 
     public abstract void initRenderer(RendererType type);
