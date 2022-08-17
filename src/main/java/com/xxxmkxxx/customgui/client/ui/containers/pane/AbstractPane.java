79326a966e33edd82a93f65321c9fbe04e625d4b
xxxmkxxx <maks88648@gmail.com>
2022-08-17 19:47:21 +0400

    Deleted nodes list

--- src/main/java/com/xxxmkxxx/customgui/client/ui/containers/pane/AbstractPane.java
+++ src/main/java/com/xxxmkxxx/customgui/client/ui/containers/pane/AbstractPane.java
@@ -5,11 +5,8 @@ import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
 import lombok.Getter;
 import net.minecraft.client.util.math.MatrixStack;
 
-import java.util.List;
-
 @Getter
 public abstract class AbstractPane extends AbstractNode implements Pane {
-    protected List<AbstractNode> nodes;
     protected MatrixStack matrixStack;
     protected Frame frame;
 }
