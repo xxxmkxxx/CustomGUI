354430206663418eb4074a962f60b6b78689abbc
xxxmkxxx <maks88648@gmail.com>
2022-08-22 18:07:14 +0400

    Added width and height fields; Added overload standard constructor

--- src/main/java/com/xxxmkxxx/customgui/client/geometry/Frame.java
+++ src/main/java/com/xxxmkxxx/customgui/client/geometry/Frame.java
@@ -1,16 +1,18 @@
 package com.xxxmkxxx.customgui.client.geometry;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class Frame {
     private final Pos startPos;
     private final Pos stopPos;
+    private final int width;
+    private final int height;
     private final boolean startInCenter;
 
     public Frame(int xPos, int yPos, int width, int height, boolean startInCenter) {
+        this.width = width;
+        this.height = height;
         this.startInCenter = startInCenter;
 
         if (startInCenter) {
@@ -20,5 +22,9 @@ public class Frame {
             this.startPos = new Pos(xPos, yPos);
             this.stopPos = new Pos(xPos + width, yPos + height);
         }
+    }
+
+    public Frame(Pos startPos, int width, int height, boolean startInCenter) {
+        this(startPos.x(), startPos.y(), width, height, startInCenter);
     }
 }
