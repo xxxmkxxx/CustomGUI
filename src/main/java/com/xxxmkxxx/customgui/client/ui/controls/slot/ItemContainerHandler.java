beba8f6654bada84c8bda58aec554fa17e1a9751
xxxmkxxx <maks88648@gmail.com>
2022-08-22 18:21:35 +0400

    Added item container handler interface

---
+++ src/main/java/com/xxxmkxxx/customgui/client/ui/controls/slot/ItemContainerHandler.java
@@ -0,0 +1,7 @@
+package com.xxxmkxxx.customgui.client.ui.controls.slot;
+
+import com.xxxmkxxx.customgui.client.common.Handler;
+
+public interface ItemContainerHandler extends Handler {
+    void onContainerUpdate();
+}
