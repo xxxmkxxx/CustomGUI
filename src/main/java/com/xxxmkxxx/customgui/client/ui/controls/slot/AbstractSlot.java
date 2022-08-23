e0944b0b6dc32b7a6e205839a651a85b3115ee2b
xxxmkxxx <maks88648@gmail.com>
2022-08-23 20:10:25 +0400

    Removed pos field

--- src/main/java/com/xxxmkxxx/customgui/client/ui/controls/slot/AbstractSlot.java
+++ src/main/java/com/xxxmkxxx/customgui/client/ui/controls/slot/AbstractSlot.java
@@ -1,7 +1,6 @@
 package com.xxxmkxxx.customgui.client.ui.controls.slot;
 
 import com.xxxmkxxx.customgui.client.geometry.Frame;
-import com.xxxmkxxx.customgui.client.geometry.Pos;
 import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,6 +10,5 @@ import lombok.Getter;
 public abstract class AbstractSlot extends AbstractNode implements Slot, ItemContainerHandler {
     protected final int slotId;
     protected final ItemContainer itemContainer;
-    protected final Pos pos;
     protected final Frame frame;
 }
