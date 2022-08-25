e514883747caebcdcd4a7100b7430cc7e756dcca
xxxmkxxx <maks88648@gmail.com>
2022-08-25 19:30:57 +0400

    Added MultiRowSlotContainer interface

--- src/main/java/com/xxxmkxxx/customgui/client/ui/containers/slotcontainer/MultiRowSlotContainer.java
+++ src/main/java/com/xxxmkxxx/customgui/client/ui/containers/slotcontainer/MultiRowSlotContainer.java
@@ -1,4 +1,11 @@
 package com.xxxmkxxx.customgui.client.ui.containers.slotcontainer;
 
-public interface MultirowSlotContainer {
+import com.xxxmkxxx.customgui.client.ui.controls.slot.AbstractSlot;
+
+import javax.naming.OperationNotSupportedException;
+
+public interface MultiRowSlotContainer<T extends AbstractSlot> {
+    AbstractRowSlotContainer<T> getRow(int index);
+    T getSlot(int rowIndex, int slotIndex);
+    void setSlot(int rowIndex, int slotIndex, T slot) throws OperationNotSupportedException;
 }
