601e7a089ee26d4c6fac56ad5f11b4a067c5aa31
xxxmkxxx <maks88648@gmail.com>
2022-08-25 19:51:38 +0400

    Changed addSlot argument slot to index; Added OperationNotSupportedException throws to methods; Added setSlot argument index

--- src/main/java/com/xxxmkxxx/customgui/client/ui/containers/slotcontainer/RowSlotContainer.java
+++ src/main/java/com/xxxmkxxx/customgui/client/ui/containers/slotcontainer/RowSlotContainer.java
@@ -2,7 +2,10 @@ package com.xxxmkxxx.customgui.client.ui.containers.slotcontainer;
 
 import com.xxxmkxxx.customgui.client.ui.controls.slot.AbstractSlot;
 
+import javax.naming.OperationNotSupportedException;
+
 public interface RowSlotContainer<T extends AbstractSlot> {
-    void addSlot(T slot);
+    void addSlot(int index) throws OperationNotSupportedException;
+    void setSlot(int index, T slot) throws OperationNotSupportedException;
     T getSlot(int index);
 }
