94b2752f1e7ef548333e6b66cb7733db39317002
xxxmkxxx <maks88648@gmail.com>
2022-08-25 20:00:52 +0400

    Added index and inventory fields; Removed redutant fields; Added getItemStack, isStandardItem, isStandardItem methods

--- src/main/java/com/xxxmkxxx/customgui/client/ui/controls/slot/AbstractSlot.java
+++ src/main/java/com/xxxmkxxx/customgui/client/ui/controls/slot/AbstractSlot.java
@@ -4,11 +4,27 @@ import com.xxxmkxxx.customgui.client.geometry.Frame;
 import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.registry.Registry;
 
 @Getter
 @AllArgsConstructor
-public abstract class AbstractSlot extends AbstractNode implements Slot, ItemContainerHandler {
-    protected final int slotId;
-    protected final ItemContainer itemContainer;
+public abstract class AbstractSlot extends AbstractNode implements Slot {
+    protected int index;
+    protected final Inventory inventory;
     protected final Frame frame;
+
+    public ItemStack getItemStack() {
+        return inventory.getStack(index);
+    }
+
+    public boolean isStandardItem() {
+        return isStandardItem(getItemStack().getItem());
+    }
+
+    public boolean isStandardItem(Item item) {
+        return Registry.ITEM.getId(item).getNamespace().equals("minecraft");
+    }
 }
