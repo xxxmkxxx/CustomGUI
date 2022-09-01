76e5ca3de55820c97d4f4687f8fcc973356e0c2c
xxxmkxxx <maks88648@gmail.com>
2022-09-01 12:41:06 +0400

    Added TargetManager class

---
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/node/TargetManager.java
@@ -0,0 +1,9 @@
+package com.xxxmkxxx.customgui.client.hierarchy.node;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class TargetManager {
+    private AbstractNode node;
+}
