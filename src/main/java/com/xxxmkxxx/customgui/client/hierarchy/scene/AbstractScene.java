43d2131419b432fcf143ef36e84298644bcf0790
xxxmkxxx <maks88648@gmail.com>
2022-09-01 13:40:17 +0400

    Refactored; Added TargetManager field; Added targetManager field init into constructor; Added updateTarget method.

--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/scene/AbstractScene.java
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/scene/AbstractScene.java
@@ -1,7 +1,10 @@
 package com.xxxmkxxx.customgui.client.hierarchy.scene;
 
 import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
+import com.xxxmkxxx.customgui.client.hierarchy.node.States;
+import com.xxxmkxxx.customgui.client.hierarchy.node.TargetManager;
 import com.xxxmkxxx.customgui.client.hierarchy.renderer.RendererType;
+import com.xxxmkxxx.customgui.client.ui.controls.slot.AbstractSlot;
 import lombok.Getter;
 
 import java.util.List;
@@ -9,10 +12,14 @@ import java.util.List;
 public abstract class AbstractScene implements Scene {
     @Getter
     protected final RendererType type;
+    @Getter
     protected List<AbstractNode> displays;
+    @Getter
+    protected final TargetManager targetManager;
 
     public AbstractScene(RendererType type) {
         this.type = type;
+        this.targetManager = new TargetManager();
     }
 
     @Override
@@ -22,7 +29,17 @@ public abstract class AbstractScene implements Scene {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void render() {
         displays.forEach(node -> node.getState().execute(node, node.getRenderer()));
+    }
+
+    public void updateTarget(int x, int y) {
+        targetManager.setNode(null);
+
+        displays.forEach(node -> {
+            if (node.getState() == States.DISPLAYED)
+                node.updateTarget(x, y, targetManager);
+        });
     }
 }
