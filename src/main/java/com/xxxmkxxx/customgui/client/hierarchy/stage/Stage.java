40533f53e546e37a7a5bef79d053fe74715b6f03
xxxmkxxx <maks88648@gmail.com>
2022-09-01 13:56:57 +0400

    Added setActiveScene, clearScene signatures

--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/stage/Stage.java
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/stage/Stage.java
@@ -1,5 +1,9 @@
 package com.xxxmkxxx.customgui.client.hierarchy.stage;
 
+import com.xxxmkxxx.customgui.client.hierarchy.scene.AbstractScene;
+
 public interface Stage {
     void render();
+    void setActiveScene(AbstractScene scene);
+    void clearScene();
 }
