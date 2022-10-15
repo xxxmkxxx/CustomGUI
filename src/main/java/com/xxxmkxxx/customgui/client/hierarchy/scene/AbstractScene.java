1e95366be89b50c0ea011603c5e5ee06e1ec191a
xxxmkxxx <maks88648@gmail.com>
2022-10-15 18:08:42 +0400

    Added renderTimeControl field; Added animationManager field; Change constructor; Change render method; Refactored.

--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/scene/AbstractScene.java
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/scene/AbstractScene.java
@@ -1,23 +1,31 @@
 package com.xxxmkxxx.customgui.client.hierarchy.scene;
 
+import com.xxxmkxxx.customgui.client.common.RenderTime;
 import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
+import com.xxxmkxxx.customgui.client.hierarchy.node.AnimationManager;
 import com.xxxmkxxx.customgui.client.hierarchy.node.TargetManager;
 import com.xxxmkxxx.customgui.client.hierarchy.renderer.RendererType;
-import com.xxxmkxxx.customgui.client.hierarchy.animations.AnimationManager;
+import com.xxxmkxxx.timecontrol.TimeControl;
 import lombok.Getter;
 
 import java.util.List;
 
 public abstract class AbstractScene implements Scene {
     @Getter
+    protected final TimeControl renderTimeControl;
+    @Getter
     protected final RendererType type;
     protected List<AbstractNode> displays;
     @Getter
     protected final TargetManager targetManager;
+    @Getter
+    protected final AnimationManager animationManager;
 
     public AbstractScene(RendererType type) {
+        this.renderTimeControl = new TimeControl(new RenderTime());
         this.type = type;
         this.targetManager = new TargetManager();
+        this.animationManager = new AnimationManager(renderTimeControl);
     }
 
     @Override
@@ -30,7 +38,7 @@ public abstract class AbstractScene implements Scene {
     @SuppressWarnings("unchecked")
     public void render() {
         displays.forEach(node -> node.getState().execute(node, node.getRenderer()));
-        AnimationManager.render();
+        renderTimeControl.tick();
     }
 
     public void updateTarget(int x, int y) {
