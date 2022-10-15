bfc488fc6db7fa8503ef3edbc4a2386b80333c21
xxxmkxxx <maks88648@gmail.com>
2022-10-15 17:46:53 +0400

    Added static dependency TimeControl

--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/animations/Animation.java
+++
@@ -1,7 +0,0 @@
-package com.xxxmkxxx.customgui.client.hierarchy.animations;
-
-public interface Animation {
-    void addPhase(AnimationPhase phase);
-    void changePhase();
-    void displayPhase();
-}
--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/animations/AnimationPhase.java
+++
@@ -1,8 +0,0 @@
-package com.xxxmkxxx.customgui.client.hierarchy.animations;
-
-import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
-
-@FunctionalInterface
-public interface AnimationPhase {
-    void display(AbstractNode node);
-}
