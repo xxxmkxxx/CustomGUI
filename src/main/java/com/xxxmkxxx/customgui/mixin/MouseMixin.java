eb7529dbaa24c4633590447d90ed921f070b1ce1
xxxmkxxx <maks88648@gmail.com>
2022-10-15 18:34:19 +0400

    Added wasLeftButtonClicked shadow method; Added client shadow field; Changed onCursorPos method; Changed onMouseButton method.

--- src/main/java/com/xxxmkxxx/customgui/mixin/MouseMixin.java
+++ src/main/java/com/xxxmkxxx/customgui/mixin/MouseMixin.java
@@ -1,8 +1,12 @@
 package com.xxxmkxxx.customgui.mixin;
 
 import com.xxxmkxxx.customgui.client.CustomGUIClient;
+import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
+import com.xxxmkxxx.customgui.client.hierarchy.node.events.click.ClickEventHandler;
 import com.xxxmkxxx.customgui.client.hierarchy.stage.state.StageState;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,10 +14,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Mouse.class)
-public class MouseMixin {
-    @Shadow private double x;
+public abstract class MouseMixin {
+    @Shadow public abstract boolean wasLeftButtonClicked();
 
-    @Shadow private double y;
+    @Shadow @Final private MinecraftClient client;
 
     @Inject(method = "lockCursor", at = @At(value = "HEAD"), cancellable = true)
     public void lockCursor(CallbackInfo ci) {
@@ -23,6 +27,25 @@ public class MouseMixin {
 
     @Inject(method = "onCursorPos", at = @At(value = "TAIL"))
     public void onCursorPos(long window, double x, double y, CallbackInfo ci) {
-        CustomGUIClient.SCREEN_STAGE.onCursorUpdate((int) x/2, (int) y/2);
+        int xPos = (int) x / 2;
+        int yPos = (int) y / 2;
+
+        if (client.getWindow().isFullscreen()) {
+            xPos = (int) x / 4;
+            yPos = (int) y / 4;
+        }
+
+        CustomGUIClient.SCREEN_STAGE.onCursorUpdate(xPos, yPos);
+    }
+
+    @Inject(method = "onMouseButton", at = @At(value = "TAIL"))
+    public void onMouseButton(long window, int button, int action, int mods, CallbackInfo ci) {
+        AbstractNode node = CustomGUIClient.SCREEN_STAGE.getActiveScene().getTargetManager().getNode();
+
+        if (node instanceof ClickEventHandler clickEventHandler) {
+            if (wasLeftButtonClicked()) {
+                clickEventHandler.callAllClickedEvents();
+            }
+        }
     }
 }
