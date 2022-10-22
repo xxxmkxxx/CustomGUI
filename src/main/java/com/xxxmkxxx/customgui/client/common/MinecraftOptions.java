ea46398c15af6720272b8420fda8abeda52bd9e7
xxxmkxxx <maks88648@gmail.com>
2022-10-22 12:46:18 +0400

    Added MinecraftOptions class

---
+++ src/main/java/com/xxxmkxxx/customgui/client/common/MinecraftOptions.java
@@ -0,0 +1,8 @@
+package com.xxxmkxxx.customgui.client.common;
+
+import net.minecraft.client.MinecraftClient;
+
+public class MinecraftOptions {
+    public static int windowWidth = MinecraftClient.getInstance().getWindow().getWidth();
+    public static int windowHeight = MinecraftClient.getInstance().getWindow().getHeight();
+}
