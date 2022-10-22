9e2d8e07180a4e7d99c80a7a4a47d86405616f5e
xxxmkxxx <maks88648@gmail.com>
2022-10-22 12:47:04 +0400

    Addded NodeSection enum

---
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/node/NodeSection.java
@@ -0,0 +1,48 @@
+package com.xxxmkxxx.customgui.client.hierarchy.node;
+
+import com.xxxmkxxx.customgui.client.geometry.frame.Frame;
+import com.xxxmkxxx.customgui.client.geometry.frame.StaticFrame;
+import com.xxxmkxxx.customgui.client.geometry.position.Pos;
+import lombok.Getter;
+
+import static com.xxxmkxxx.customgui.client.common.MinecraftOptions.windowHeight;
+import static com.xxxmkxxx.customgui.client.common.MinecraftOptions.windowWidth;
+
+@Getter
+public enum NodeSection {
+    TOP(
+            new Pos(0, 0),
+            new Pos(windowWidth, windowHeight / 4)
+    ),
+
+    BOTTOM(
+            new Pos(0, (windowHeight / 4) * 3),
+            new Pos(windowWidth, windowHeight)
+    ),
+
+    LEFT(
+            new Pos(0, windowHeight / 4),
+            new Pos(windowWidth / 3, (windowHeight / 4) * 3)
+    ),
+
+    RIGHT(
+            new Pos((windowWidth / 3) * 2, windowHeight / 4),
+            new Pos(windowWidth, (windowHeight / 4) * 3)
+    ),
+
+    CENTER(
+            new Pos(windowWidth / 3, windowHeight / 4),
+            new Pos((windowWidth / 3) * 2, (windowHeight / 4) * 3)
+    ),
+
+    MIXED(
+            new Pos(0, 0),
+            new Pos(windowWidth, windowHeight)
+    );
+
+    private Frame frame;
+
+    NodeSection(Pos startPos, Pos stopPos) {
+        frame = new StaticFrame(startPos, stopPos, false);
+    }
+}
