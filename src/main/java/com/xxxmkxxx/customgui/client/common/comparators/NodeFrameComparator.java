2c12e853de170e4780d33b8d5db6b9324af7afe0
xxxmkxxx <maks88648@gmail.com>
2022-10-22 21:34:03 +0400

    Added src/main/java/com/xxxmkxxx/customgui/client/common/comparators/

---
+++ src/main/java/com/xxxmkxxx/customgui/client/common/comparators/NodeFrameComparator.java
@@ -0,0 +1,12 @@
+package com.xxxmkxxx.customgui.client.common.comparators;
+
+import com.xxxmkxxx.customgui.client.hierarchy.node.AbstractNode;
+
+import java.util.Comparator;
+
+public class NodeFrameComparator implements Comparator<AbstractNode> {
+    @Override
+    public int compare(AbstractNode o1, AbstractNode o2) {
+        return o1.getFrame().getDiagonal() - o2.getFrame().getDiagonal();
+    }
+}
