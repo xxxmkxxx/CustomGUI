1ad07bec9bd3dc9ca6c8af9b0a7b44b0d8cdb41d
xxxmkxxx <maks88648@gmail.com>
2023-02-04 17:13:29 +0400

    Added move methods for Pos.
    Replace record to class;
    Refactored.

--- src/main/java/com/xxxmkxxx/customgui/client/hierarchy/window/position/Pos.java
+++ src/main/java/com/xxxmkxxx/customgui/client/hierarchy/window/position/Pos.java
@@ -1,16 +1,55 @@
 package com.xxxmkxxx.customgui.client.hierarchy.window.position;
 
-public record Pos(int x, int y) {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Pos {
     public static Pos DEFAULT_POS = new Pos(10, 10);
+    private int x;
+    private int y;
+
+    public Pos moveByX(int distance) {
+        x += distance;
+        return this;
+    }
+
+    public Pos moveByY(int distance) {
+        y += distance;
+        return this;
+    }
+
+    public static Pos moveByX(Pos pos, int distance) {
+        pos.moveByX(distance);
+        return pos;
+    }
+
+    public static Pos moveByY(Pos pos, int distance) {
+        pos.moveByY(distance);
+        return pos;
+    }
+
+    public Pos moveByXY(int xDistance, int yDistance) {
+        x += xDistance;
+        y += yDistance;
+        return this;
+    }
+
+    public static Pos moveByXY(Pos pos, int xDistance, int yDistance) {
+        pos.moveByXY(xDistance, yDistance);
+        return pos;
+    }
+
     public int calculateSegmentLength(Pos secondPos) {
         return calculateSegmentLength(this, secondPos);
     }
 
     public static int calculateSegmentLength(Pos firstPos, Pos secondPos) {
-        int differenceOnX = secondPos.x() - firstPos.x();
-        int differenceOnY = secondPos.y() - firstPos.y();
+        int differenceOnX = secondPos.getX() - firstPos.getX();
+        int differenceOnY = secondPos.getY() - firstPos.getY();
 
-        return Math.abs(differenceOnX * differenceOnX + differenceOnY * differenceOnY);
+        return (int) Math.sqrt(differenceOnX * differenceOnX + differenceOnY * differenceOnY);
     }
 
     @Override
