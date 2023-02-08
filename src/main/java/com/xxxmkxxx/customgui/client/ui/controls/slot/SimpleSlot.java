4cdd379b380369d03721424f2c6656b8c0270cfb
xxxmkxxx <maks88648@gmail.com>
2023-02-08 15:14:54 +0400

    Refactored.
    Integrated SimpleText into SimpleSlot;
    Integrated SimpleImage into SimpleSlot.

--- src/main/java/com/xxxmkxxx/customgui/client/ui/controls/slot/SimpleSlot.java
+++ src/main/java/com/xxxmkxxx/customgui/client/ui/controls/slot/SimpleSlot.java
@@ -2,11 +2,9 @@ package com.xxxmkxxx.customgui.client.ui.controls.slot;
 
 import com.xxxmkxxx.customgui.CustomGUI;
 import com.xxxmkxxx.customgui.client.common.ParametrizedSelfDestructionMethod;
+import com.xxxmkxxx.customgui.client.common.event.EventBus;
 import com.xxxmkxxx.customgui.client.common.inventory.AbstractInventory;
 import com.xxxmkxxx.customgui.client.common.util.Utils;
-import com.xxxmkxxx.customgui.client.hierarchy.window.frame.SimpleFrame;
-import com.xxxmkxxx.customgui.client.hierarchy.window.position.Pos;
-import com.xxxmkxxx.customgui.client.common.event.EventBus;
 import com.xxxmkxxx.customgui.client.hierarchy.node.events.click.LeftClickEventHandler;
 import com.xxxmkxxx.customgui.client.hierarchy.node.events.hovere.HoverEventHandler;
 import com.xxxmkxxx.customgui.client.hierarchy.node.events.hovere.ResetHoverEventHandler;
@@ -15,6 +13,8 @@ import com.xxxmkxxx.customgui.client.hierarchy.renderer.NodeRendererFactory;
 import com.xxxmkxxx.customgui.client.hierarchy.renderer.RendererType;
 import com.xxxmkxxx.customgui.client.hierarchy.style.Background;
 import com.xxxmkxxx.customgui.client.hierarchy.style.Style;
+import com.xxxmkxxx.customgui.client.hierarchy.window.position.Pos;
+import com.xxxmkxxx.customgui.client.ui.controls.image.SimpleImage;
 import com.xxxmkxxx.customgui.client.ui.controls.text.SimpleText;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -30,23 +30,65 @@ import java.util.function.Consumer;
 @Getter
 @SuppressWarnings("unused")
 public class SimpleSlot extends AbstractSlot implements LeftClickEventHandler, HoverEventHandler, ResetHoverEventHandler {
+    private SimpleText amountItemsText;
+    private SimpleImage image;
     protected Runnable leftClickAction;
     protected Runnable hoverAction;
     protected Runnable resetHoverAction;
 
-    protected SimpleSlot(int index, AbstractInventory inventory, SimpleFrame frame) {
-        super(index, inventory, frame);
+    protected SimpleSlot(Pos startPos, int width, int height, int index, AbstractInventory inventory) {
+        super(startPos, width, height, index, inventory);
         this.leftClickAction = () -> {};
         this.hoverAction = () -> {};
         this.resetHoverAction = () -> {};
-        this.amountItemsText = SimpleText.builder().style(style).build();
-        updateAmountItemsText(inventory.getStack(index), frame);
+        this.amountItemsText = SimpleText.builder()
+                .pos(initAmountItemsTextStartPos(Text.of(String.valueOf(inventory.getStack(index).getCount()))))
+                .style(new Style())
+                .build();
+        this.image = SimpleImage.builder()
+                .startPos(startPos)
+                .width(width)
+                .height(height)
+                .style(new Style())
+                .build();
+
+        updateAmountItemsText(inventory.getStack(index));
+    }
+
+    private Pos initAmountItemsTextStartPos(Text text) {
+        return new Pos(
+                frame.getStopPos().getX() - Utils.getTextWidth(text),
+                frame.getStopPos().getY() - Utils.getTextHeight()
+        );
+    }
+
+    private void updateAmountItemsText(ItemStack itemStack) {
+        Text amount = Text.of(Integer.toString(itemStack.getCount()));
+
+        Pos startPos = initAmountItemsTextStartPos(amount);
+
+        Pos stopPos = new Pos(
+                startPos.getX() + amountItemsText.getTextWidth(),
+                frame.getStopPos().getY() + amountItemsText.getTextHeight()
+        );
+
+        amountItemsText.getFrame().moveStartPos(startPos);
+        amountItemsText.getFrame().moveStopPos(stopPos);
+        amountItemsText.setText(amount);
+    }
+
+    @Override
+    public void scaling(double widthScaleValue, double heightScaleValue) {
+        super.scaling(widthScaleValue, heightScaleValue);
+        amountItemsText.scaling(widthScaleValue, heightScaleValue);
+        image.scaling(widthScaleValue, heightScaleValue);
     }
 
     @Override
     public void initRenderer(RendererType type) {
         super.initRenderer(type);
         amountItemsText.initRenderer(type);
+        image.initRenderer(type);
         this.renderer = new RendererFactory().create(type);
     }
 
@@ -86,21 +128,28 @@ public class SimpleSlot extends AbstractSlot implements LeftClickEventHandler, H
         return new Identifier(itemIdentifier.getNamespace(), "textures/gui/" + itemIdentifier.getPath());
     }
 
+    public static FactoryBuilder factoryBuilder(int width, int height, AbstractInventory inventory) {
+        return new FactoryBuilder(width, height, inventory);
+    }
+
+    public static Factory factory(Pos startPos, int width, int height, AbstractInventory inventory) {
+        return new Factory(width, height, inventory);
+    }
+
     @Setter
     @RequiredArgsConstructor
     public static class Factory implements SlotFactory<SimpleSlot> {
         private final int width;
         private final int height;
-        private final int backgroundColor;
         private final AbstractInventory inventory;
-        private Style style;
+        private Style style = Style.DEFAULT_STYLE;
         private Consumer<ItemStack> leftClickAction = (itemStack) -> {};
         private Runnable hoverAction = () -> {};
         private Runnable resetHoverAction = () -> {};
 
         @Override
         public SimpleSlot create(int index, Pos pos) {
-            SimpleSlot slot = new SimpleSlot(index, inventory, new SimpleFrame(pos, width, height));
+            SimpleSlot slot = new SimpleSlot(pos, width, height, index, inventory);
             slot.setLeftClickAction(() -> leftClickAction.accept(inventory.getStack(index)));
             slot.setHoverAction(hoverAction);
             slot.setResetHoverAction(resetHoverAction);
@@ -113,8 +162,8 @@ public class SimpleSlot extends AbstractSlot implements LeftClickEventHandler, H
     public static class FactoryBuilder {
         private Factory factory;
 
-        public FactoryBuilder(int width, int height, int backgroundColor, AbstractInventory inventory) {
-            this.factory = new Factory(width, height, backgroundColor, inventory);
+        public FactoryBuilder(int width, int height, AbstractInventory inventory) {
+            this.factory = new Factory(width, height, inventory);
         }
 
         public FactoryBuilder leftClickAction(Consumer<ItemStack> leftClickAction) {
@@ -145,8 +194,7 @@ public class SimpleSlot extends AbstractSlot implements LeftClickEventHandler, H
     public static class RendererFactory implements NodeRendererFactory<SimpleSlot> {
         private ParametrizedSelfDestructionMethod<SimpleSlot> initBackgroundMethod = new ParametrizedSelfDestructionMethod<>();
         private Consumer<SimpleSlot> backgroundRenderMethod = simpleSlot -> {};
-        private Consumer<SimpleSlot> standardImageRenderMethod = simpleSlot -> {};
-        private Consumer<SimpleSlot> customImageRenderMethod = simpleSlot -> {};
+        private Consumer<SimpleSlot> standardImageRenderMethod;
 
         public RendererFactory() {
             initBackgroundMethod.setAction(simpleSlot -> {
@@ -156,13 +204,6 @@ public class SimpleSlot extends AbstractSlot implements LeftClickEventHandler, H
                 CustomGUI.NODE_DRAWABLE_HELPER.drawTexture(
                         simpleSlot.getItemStack(),
                         simpleSlot.getFrame()
-                );
-            };
-            customImageRenderMethod = simpleSlot -> {
-                CustomGUI.NODE_DRAWABLE_HELPER.drawTexture(
-                        simpleSlot.getMatrixStack(),
-                        simpleSlot.getFrame(),
-                        simpleSlot.getItemAtlas(simpleSlot.getItemStack().getItem())
                 );
             };
         }
@@ -179,10 +220,10 @@ public class SimpleSlot extends AbstractSlot implements LeftClickEventHandler, H
 
             ItemStack itemStack = slot.getItemStack();
             if (itemStack == ItemStack.EMPTY) return;
-            if (slot.isStandardItem()) standardImageRenderMethod.accept(slot);
-            else customImageRenderMethod.accept(slot);
+            if (slot.getImage().isStandard()) standardImageRenderMethod.accept(slot);
+            else slot.getImage().getState().execute(slot.getImage(), slot.getImage().getRenderer());
 
-            slot.getAmountItemsText().getRenderer().render(slot.getAmountItemsText());
+            slot.getAmountItemsText().getState().execute(slot.getAmountItemsText(), slot.getAmountItemsText().getRenderer());
         }
     }
 }
