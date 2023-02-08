02edb641a5c2fe818d21488feee5e325a3f56732
xxxmkxxx <maks88648@gmail.com>
2023-02-08 14:15:12 +0400

    Refactored.

--- src/main/java/com/xxxmkxxx/customgui/client/CustomGUIClient.java
+++ src/main/java/com/xxxmkxxx/customgui/client/CustomGUIClient.java
@@ -3,6 +3,7 @@ package com.xxxmkxxx.customgui.client;
 import com.xxxmkxxx.customgui.CustomGUI;
 import com.xxxmkxxx.customgui.client.common.inventory.AbstractInventory;
 import com.xxxmkxxx.customgui.client.common.inventory.InventoryType;
+import com.xxxmkxxx.customgui.client.common.util.Utils;
 import com.xxxmkxxx.customgui.client.hierarchy.renderer.RendererType;
 import com.xxxmkxxx.customgui.client.hierarchy.scene.SimpleScene;
 import com.xxxmkxxx.customgui.client.hierarchy.style.Background;
@@ -12,6 +13,7 @@ import com.xxxmkxxx.customgui.client.hierarchy.style.Style;
 import com.xxxmkxxx.customgui.client.hierarchy.window.position.Pos;
 import com.xxxmkxxx.customgui.client.ui.controls.button.ImagedButton;
 import com.xxxmkxxx.customgui.client.ui.controls.button.SimpleButton;
+import com.xxxmkxxx.customgui.client.ui.controls.field.InputField;
 import com.xxxmkxxx.customgui.client.ui.controls.image.SimpleImage;
 import com.xxxmkxxx.customgui.client.ui.controls.slot.SimpleSlot;
 import com.xxxmkxxx.customgui.client.ui.controls.text.SimpleText;
@@ -24,6 +26,7 @@ import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 
@@ -85,22 +88,12 @@ public class CustomGUIClient implements ClientModInitializer {
         ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
             CustomGUI customGUI = CustomGUI.getInstance();
 
-            customGUI.addGUIBlock("test", (hudStage, screenStage) -> {
+            Style style = new Style();
+            style.setColor(new Color("2ad43b"));
+            style.setBackground(Background.builder().opacity(new Opacity(70)).type(Background.Type.COLORED).color(Color.DefaultColor.BLUE.getColor()).build());
+
+            customGUI.addGUIBlock("testButton", (hudStage, screenStage) -> {
                 SimpleScene scene = new SimpleScene(RendererType.SCREEN);
-
-                Style style = new Style();
-                style.setColor(new Color("2ad43b"));
-                style.setBackground(Background.builder().opacity(new Opacity(100)).type(Background.Type.COLORED).color(new Color("142615")).build());
-                style.setOpacity(new Opacity(80));
-
-                SimpleImage image = SimpleImage.builder()
-                        .startPos(new Pos(150, 150))
-                        .width(20).height(20)
-                        .identifier(new Identifier("customgui", "textures/gui/img_1.png"))
-                        .build();
-                image.setLeftClickAction(() -> {
-                    MinecraftClient.getInstance().player.sendMessage(Text.of("Iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiimage"), false);
-                });
 
                 SimpleButton button = SimpleButton.builder()
                         .startPos(new Pos(150, 150))
@@ -111,40 +104,48 @@ public class CustomGUIClient implements ClientModInitializer {
                 });
                 button.setStyle(style);
 
-                ImagedButton imagedButton = ImagedButton.builder()
-                        .image(image)
-                        .text("aboba")
-                        .pos(new Pos(150, 150))
-                        .style(style)
-                        .build();
-
-                SimpleText simpleText = SimpleText.builder()
-                        .pos(image.getFrame().getStopPos())
-                        .style(style)
-                        .build();
-
-                /*EventBus.CHANGE_FRAME_EVENT.addHandler(button.getFrame(), (ChangeFrameEventHandler) () -> {
-                    ((DynamicFrame)image.getFrame()).setStopPos(button.getFrame().getStopPos());
-                });*/
-
                 //scene.addElement(image);
                 //scene.addElement(button);
-                //scene.addElement(simpleText);
-                scene.addElement(imagedButton);
+                scene.addElement(button);
 
                 return scene;
             });
-            customGUI.addGUIBlock("test2", (hudStage, screenStage) -> {
+            customGUI.addGUIBlock("testSlot", (hudStage, screenStage) -> {
                 SimpleScene scene = new SimpleScene(RendererType.SCREEN);
 
-                SimpleSlot.Factory slotFactory = SimpleSlot.factoryBuilder(18, 18,inventory).build();
+                SimpleSlot.Factory slotFactory = SimpleSlot.factoryBuilder(20, 20, inventory).build();
+                slotFactory.setLeftClickAction(itemStack -> {
+                    MinecraftClient.getInstance().player.sendMessage(itemStack.getItem().getName(), false);
+                });
 
                 scene.addElement(slotFactory.create(2, new Pos(50, 50)));
 
                 return scene;
             });
+            customGUI.addGUIBlock("testLabel", (hudStage, screenStage) -> {
+                SimpleScene scene = new SimpleScene(RendererType.SCREEN);
 
-            customGUI.setActiveScene("test2", RendererType.SCREEN);
+                InputField field = InputField.builder().build(new Pos(50, 50), 20, Utils.getTextHeight());
+                scene.addElement(field);
+
+                return scene;
+            });
+            customGUI.addGUIBlock("testInputField", (hudStage, screenStage) -> {
+                SimpleScene scene = new SimpleScene(RendererType.SCREEN);
+
+                Style inputStyle = new Style();
+                inputStyle.setColor(new Color("2ad43b"));
+                inputStyle.setBackground(Background.builder().opacity(new Opacity(70)).type(Background.Type.COLORED).color(Color.DefaultColor.BLUE.getColor()).build());
+
+                InputField inputField = InputField.builder().style(inputStyle).build(new Pos(50, 50), 40, Utils.getTextHeight());
+                inputField.getInputCursor().getStyle().setColor(Color.DefaultColor.ORANGE.getColor());
+
+                scene.addElement(inputField);
+
+                return scene;
+            });
+
+            customGUI.setActiveScene("testSlot", RendererType.SCREEN);
         });
     }
 }
